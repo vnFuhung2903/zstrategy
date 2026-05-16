97c8828d1bfedf0c3db96bb947a4bc84db43f683
vnFuhung2903 <hung29032004@gmail.com>
2026-05-16 21:54:06 +0700

    refactor: add dotenv configuration and dependency for gas reporting

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-contract-sizer";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -39,6 +41,12 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: "typechain-types",
     target: "ethers-v6",
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS === "true",
+    currency: "USD",
+    L1: "ethereum",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
 };
 
